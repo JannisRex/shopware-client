311118286ebc883be3b9417092db9f4230f27c39
jannisrex <rex.jannis@web.de>
2023-03-07 15:00:01 +0100

    [fix] static serving and fileDir
    Signed-off-by: jannisrex <rex.jannis@web.de>

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from 'express'
-import path from 'path'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+app.use(express.static(path.join(__dirname, 'src', 'static')))
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve('src', 'index.html'))
+  res.sendFile(path.resolve('./src/static', 'index.html'))
 })
 
 app.listen(process.env.PORT || 3000, () => console.log('App started!'))
